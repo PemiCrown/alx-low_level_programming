b07160e85719c068c535fccb583cd15c3013c723
Oluwapemi Adekunle <adekunleoluwapemi@gmail.com>
2022-12-10 09:22:44 -0800

    To print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - main block
- *return: 0
+ * main-Entry point
+ *return: 0 if successful
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
