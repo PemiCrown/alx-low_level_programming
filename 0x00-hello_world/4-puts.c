66aa9d39372bf26abe6821ff19d494b33b621734
Oluwapemi Adekunle <adekunleoluwapemi@gmail.com>
2022-12-10 09:57:57 -0800

    To print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main-Entry point
- *return: 0 if successful
+ * main - Entry point
+ * Description: Prints the text
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
