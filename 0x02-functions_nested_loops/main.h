087d2caed2f0978d7d1baad0d86015bb8e5d4401
Oluwapemi Adekunle <adekunleoluwapemi@gmail.com>
2022-12-17 03:30:39 -0800

    Print putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,14 @@
-#ifndef int _putchar(char c);
-#ifndef void print_alphabet(void);
-#ifndef void print_alphabet_x10(void);
-#ifndef int _islower(int c);
-#ifndef int _isalpha(int c);
-#ifndef int print_sign(int n);
-#ifndef int _abs(int);
-#ifndef int print_last_digit(int);
-#ifndef void jack_bauer(void);
-#ifndef void times_table(void);
-#ifndef int add(int, int);
-#ifndef void print_to_98(int n);
-#ifndef void print_times_table(int n);
-#endif
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
