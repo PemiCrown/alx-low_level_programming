3e72c35127e669203d867a608c18765773426b9d
Oluwapemi Adekunle <adekunleoluwapemi@gmail.com>
2022-12-19 05:13:40 -0800

    Convert a date to the day of year and determines how many days are left in the year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,13 +10,13 @@
  *Return: void
  */
 
-void print_remaining_days(int month, int day, int year)  
+void print_remaining_days(int month, int day, int year)
 {
 if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 {
 if (month >= 3 && day >= 60)
 {
-day++ ;
+day++;
 }
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
