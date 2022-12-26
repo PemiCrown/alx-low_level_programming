b6e5c46a2e5f9ca299be95bfad4667ff48219784
Oluwapemi Adekunle <adekunleoluwapemi@gmail.com>
2022-12-25 23:43:06 -0800

    Function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,7 @@
 
 /**
  * print_number - print an integer using only _putchar
- * @number: integer to be printed by function
+ * @n: integer to be printed by function
  *
  * Return: nothing
  **/
@@ -15,10 +15,10 @@ if (n < 0)
 {
 n *= -1;
 k = n;
- _putchar('-');
+_putchar('-');
 }
 k /= 10;
- if (k != 0)
+if (k != 0)
 print_number(k);
 _putchar((unsigned int) n % 10 + '0');
 }
